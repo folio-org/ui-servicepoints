d080da21e61f74a375a573810d780b7fd108a798
Michal Kuklis <michal.kuklis@gmail.com>
2018-08-22 11:41:44 -0400

    Add service point modal

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import events from '@folio/stripes-core/src/events';
+import { get } from 'lodash';
+
+import ServicePointModal from './ServicePointModal';
 
 export default class ServicePoints extends React.Component {
-
   static eventHandler(event, stripes, data = {}) {
-    // TODO finalize this
-    if (event === events.LOGIN) {
+    const { user } = stripes;
+
+    if (event === events.LOGIN && !get(user, ['user', 'curServicePoint'])) {
       return ServicePoints;
     }
   }
 
   render() {
-    return (<div></div>);
+    const { stripes } = this.props;
+    const servicePoints = get(stripes, ['user', 'user', 'servicePoints'], []);
+    const curServicePoint = get(stripes, ['user', 'user', 'curServicePoint']);
+
+    return (
+      <ServicePointModal
+        open={!curServicePoint}
+        stripes={stripes}
+        servicePoints={servicePoints}
+      />
+    );
   }
 }
