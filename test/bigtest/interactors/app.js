0af97df15b194cfbcbf673d6b04e8d6d4cdc4d31
Kyle Felker <felkerk@gvsu.edu>
2020-07-28 10:09:30 -0400

    removing unused interactor

--- test/bigtest/interactors/app.js
+++ test/bigtest/interactors/app.js
@@ -4,15 +4,11 @@ import {
   clickable,
 } from '@bigtest/interactor';
 
-import ProfileNavInteractor from './profileNavInteractor';
-
 const navSelector = '[class^="navRoot---"]';
 
 @interactor class AppInteractor {
   nav = collection(title => `${navSelector} a[aria-label="${title}"]`);
   clickOnMenuDropdown = clickable('#profileDropdown');
-
-  profileNav = new ProfileNavInteractor();
 }
 
 export default AppInteractor;
